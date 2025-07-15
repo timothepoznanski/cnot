6e0e04e1d8e453758abbc5c9cc4a4b6c46622dce
Timothé Poznanski <your_email@example.com>
2025-07-15 22:29:04 +0200

    Refactor HTML: remove unused Bootstrap CSS link and commented Google Fonts link

--- src/trash.php
+++ src/trash.php
@@ -13,8 +13,6 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
 	<title><?php echo JOURNAL_NAME;?></title>
-	<link type="text/css" rel="stylesheet" href="css/bootstrap.css"/>
-	<!-- <link href='https://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,400,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'> -->
 	<link type="text/css" rel="stylesheet" href="css/style.css"/>	
 	<link rel="stylesheet" href="css/font-awesome.css" />
 	<link rel="stylesheet" type="text/css" href="css/popline.css" />

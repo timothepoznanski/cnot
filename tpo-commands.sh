840ae70bd0b35f84884098c6a46fa65bc295a758
Timothé Poznanski <your_email@example.com>
2025-07-21 03:06:11 +0200

    Add git checkout command before pulling updates in production installation script

--- tpo-commands.sh
+++ tpo-commands.sh
@@ -5,6 +5,7 @@ docker compose -f docker-compose.yml -f docker-compose-reverse-proxy.yml up -d -
 docker compose -p cnot-dev --env-file .env.dev -f docker-compose.yml -f docker-compose-dev.yml -f docker-compose-reverse-proxy-dev.yml up -d --build
 
 # Update Prod
+git checkout main
 cd /root/cnot/cnot
 git pull
 docker compose -f docker-compose.yml -f docker-compose-reverse-proxy.yml build --no-cache

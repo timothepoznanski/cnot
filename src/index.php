4ea4e9911daa620944711ecdf07119397dadc4b0
Timothé Poznanski <your_email@example.com>
2025-07-21 18:25:22 +0200

    Test deployment workflow: Add coffee emoji to Support button ☕

--- src/index.php
+++ src/index.php
@@ -236,7 +236,7 @@ if($note != '') {
                     </div>
                     <div class="settings-menu-item" onclick="koFiAction();">
                         <i class="fas fa-coffee"></i>
-                        <span>Support me</span>
+                        <span>Support me ☕</span>
                     </div>
                     <div class="settings-menu-item deployment-info">
                         <i class="fas fa-rocket"></i>
